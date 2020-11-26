4be754e88254de230d97ef411c68969d718f69a7
Mudassar Hanif <mudassarhanif2@gmail.com>
2020-11-26 11:32:07 -0800

    completed project

---
+++ public_/build/precache-manifest.81e51729dc6f47edeae6ab45eaaa1f5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b814b37ec3e0fb0380291a8824baaafc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6870d85c58df02a88a61",
+    "url": "/static/css/main.3607a45b.chunk.css"
+  },
+  {
+    "revision": "897767d68adbfb9977c9",
+    "url": "/static/js/2.637d53ee.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/static/js/2.637d53ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6870d85c58df02a88a61",
+    "url": "/static/js/main.2df63889.chunk.js"
+  },
+  {
+    "revision": "3c723e48decc6f6f72c1",
+    "url": "/static/js/runtime-main.30aace72.js"
+  }
+]);
