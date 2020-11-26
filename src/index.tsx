61d505bba296709c93c6d267b8085f8684137ffa
Mudassar Hanif <mudassarhanif2@gmail.com>
2020-11-26 09:03:32 -0800

    updated css

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as serviceWorker from './service-worker';
 
+
+if('serviceWorker' in navigator){
+    navigator.serviceWorker
+    .register('service-worker.js')
+    .then(registration => {
+        console.log("Service Worker registered!");
+    })
+    .catch(error => {
+        console.log("error registering service worker! Error is:", error);
+    });
+}
 
 ReactDOM.render(<App/>, document.getElementById('root'));
+
+serviceWorker.register();
---
+++ src/sw.js
@@ -0,0 +1,10 @@
+
+
+self.addEventListener('install', (event) => {
+    // do something after install'
+})
+
+
+self.addEventListener('activate', (event) => {
+    //do something upon activation
+})
